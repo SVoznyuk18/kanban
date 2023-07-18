2c2f319df19ada19631527d42e3fdc6acbc6b205
Sergey.V <s.voznyuk18@gmail.com>
2023-07-18 15:54:04 +0300

    toggle sidebar button

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,32 @@
-import { SidebarSection, Title } from './sidebar.style';
+'use client';
 
-const Sidebar = () => {
+import React, { useState } from 'react';
+
+import Image from 'next/image';
+
+import { SidebarSection, Title, ToggleSideBar } from './sidebar.style';
+
+
+const Sidebar: React.FC = () => {
+
+  const [toggleSidebar, setToggleSidebar] = useState<boolean>(false);
+
+  console.log(toggleSidebar)
+
   return (
-    <SidebarSection>
+    <SidebarSection isHiddeSidebar={toggleSidebar}>
       <Title>Hello</Title>
+      <ToggleSideBar
+        onClick={() => setToggleSidebar(!toggleSidebar)}
+      >
+        <Image
+          width='20'
+          height='13'
+          src='/show-sidebar.svg'
+          alt='showSidebarIcon'
+        />
+
+      </ToggleSideBar>
     </SidebarSection>
   )
 }
--- src/components/Sidebar/sidebar.style.tsx
+++ src/components/Sidebar/sidebar.style.tsx
@@ -2,12 +2,39 @@
 
 import styled from 'styled-components'
 
-export const SidebarSection = styled.div`
+export const SidebarSection = styled.div <{ isHiddeSidebar: boolean }>`
   width: 300px;
   height: 100vh;
+  background-color: aqua;
+  transition: width 0.2s;
+  ${({ isHiddeSidebar }) => isHiddeSidebar && `
+    width: 0px;
+    overflow: hidden;
+    transition: width 0.2s;
+  `}
 `;
 
 export const Title = styled.h1`
   font-size: ${({ theme }) => theme.fontSize.xl};
   color: red;
+`;
+
+export const ToggleSideBar = styled.button`
+  position: fixed;
+  left: 0;
+  bottom: 32px;
+  width: 56px;
+  height: 48px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  border-top-right-radius: 24px;
+  border-bottom-right-radius: 24px;
+  background-color: #635FC7;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #A8A4FF;
+  }
 `;
