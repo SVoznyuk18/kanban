9c664a2370303461fff9b5ecb8068f0ce488b98f
Vozniuk Serhii <s.voznyuk18@gmail.com>
2023-09-14 16:45:13 +0300

    add api to get sub tasks by board id

--- app/api/subtasks/route.ts
+++ app/api/subtasks/route.ts
@@ -3,6 +3,20 @@ import { NextResponse, NextRequest } from "next/server"
 import { connectMongoDB } from "@/LibRoot";
 import { Subtask } from '@/ModelsRoot'
 
+export async function GET(req: NextRequest) {
+  const query = req.nextUrl.searchParams.get('mainBoardId');
+  await connectMongoDB();
+
+  const subtasks = await Subtask.find().where({ mainBoardId: query }).exec();
+
+  if (!subtasks) {
+    return NextResponse.json({ success: false, msg: "Failed to get subtasks" }, {
+      status: 404
+    })
+  }
+  return NextResponse.json({ success: true, result: subtasks });
+}
+
 export async function POST(req: Request) {
   await connectMongoDB();
   const { mainBoardId, subTasks, mainTaskId } = await req.json();
